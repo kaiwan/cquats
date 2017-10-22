970e0c773dd8b0ee0365eab8beabd6851dcc3170
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2017-10-22 12:06:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# cquats
-CQuATS is a management system that aims to collect heterogeneous code safety and security analysers/tools (primarily for 'C' code), bringing them together into a coherent, easy-to-use system.
+CQuATS - C QA ToolSet project
+
+CQuATS is a management system that aims to collect heterogeneous code safety
+and security analysers/tools (primarily for 'C' code), bringing them together
+into a coherent, easy-to-use system.
 
 Categories:
 
---
+++ cquats_common.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# cquats_common.sh
+# 
+# Quick Description:
+# CQuATS - C QA ToolSet project
+# 
+# Common 'header' file.
+#
+# Last Updated :
+# Created      : 21Sep2017
+# 
+# Author:
+# Kaiwan N Billimoria
+# kaiwan -at- kaiwantech -dot- com
+# kaiwanTECH
+# 
+# License:
+# MIT License.
+# 
+
+########### Globals follow #########################
+# Style: gNameOfGlobalVar
+
+set -a   # export all
+
+gPRJ="CQuATS - the C/C++ QA ToolSet"
+gInitMsg=\
+"
+@@@ ${name}: WARNING! Don't blindly act on \"${gPRJ}\"'s output @@@
+@@@ False positives can and do occur. Carefully audit and Verify your code.    @@@
+@@@ (Also, this toolset works only on C source/header files, nothing else)     @@@
+"
+
+TOP=$(pwd)
+MENUDIR=${TOP}/kbuild
+CONFIG_FILE=${MENUDIR}/.config
+
+gCQUATS_SCRATCH_FOLDER=${TOP}/.scratch_cquats
+mkdir -p ${gCQUATS_SCRATCH_FOLDER} || FatalError "Creating scratch folder \"${gCQUATS_SCRATCH_FOLDER}\""
+gTMPF_CONFIG=${gCQUATS_SCRATCH_FOLDER}/.tmpconfig
+
+LOGFILE_TOOLS=${TOP}/log_tools_output.txt
+DT_LOG="[$(date +%a_%d%b%Y_%T.%N)]"
+
+TOOLS_LOC=${TOP}/tools
+TOOLS_STATIC=${TOOLS_LOC}/static
+
+#----------------------------------------------------------------------
+LN="-------------------------------------------------------------------"
+echolog()
+{
+#[ $# -eq 0 ] && return
+echo "${LN}
+[$(date +%a_%d%b%Y_%T.%N)]:$@"
+#${LN}"
+}
+
+#----------------------------------------------------------------------
+report_progress()
+{
+local frame=1
+printf "$(date +%F.%H%M%S):${BASH_SOURCE[${frame}]}:${FUNCNAME[${frame}]}:${BASH_LINENO[0]}\n"
+}
+
+# Run this func in the background
+progress_wheel()
+{
+while [ true ]
+do
+  for item in \| \/ \- \\ \| \/ \- \\
+  do
+    printf "      %c\r" ${item}
+    sleep 0.2
+  done
+done
+}
